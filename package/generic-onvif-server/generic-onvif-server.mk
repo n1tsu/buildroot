d7b4ed6d0e3268d1d3d3df180f1f320dba032a35
Augustin Thiercelin <augustin.thiercelin@outlook.com>
2021-05-05 23:25:03 +0200

    package/generic-onvif-server: update package files

--- package/generic-onvif-server/generic-onvif-server.mk
+++ package/generic-onvif-server/generic-onvif-server.mk
@@ -5,10 +5,11 @@
 ################################################################################
 
 GENERIC_ONVIF_SERVER_VERSION = e2f3f35a28555357e35476927ec6a122870a4459
-GENERIC_ONVIF_SERVER_SITE = $(call github,n1tsu,generic-onvif-server,v$(GENERIC_ONVIF_SERVER_VERSION))
+GENERIC_ONVIF_SERVER_SITE = $(call github,n1tsu,generic-onvif-server,$(GENERIC_ONVIF_SERVER_VERSION))
+GENERIC_ONVIF_SERVER_DEPENDENCIES = gstreamer1 gst1-rtsp-server
 
 define GENERIC_ONVIF_SERVER_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
 endef
 
 define GENERIC_ONVIF_SERVER_TARGET_CMDS
